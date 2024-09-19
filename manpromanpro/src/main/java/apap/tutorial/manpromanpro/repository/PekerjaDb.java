112568df86fb22209c35a626f5bbb710f65a5943
Haramchu <clementsamuelm@gmail.com>
2024-09-20 01:05:34 +0700

    feat: tutorial 3 - add repository

---
+++ manpromanpro/src/main/java/apap/tutorial/manpromanpro/repository/PekerjaDb.java
@@ -0,0 +1,11 @@
+package apap.tutorial.manpromanpro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import apap.tutorial.manpromanpro.model.Pekerja;
+
+@Repository
+public interface PekerjaDb extends JpaRepository<Pekerja, Long> {
+
+}
---
+++ manpromanpro/src/main/java/apap/tutorial/manpromanpro/repository/ProyekDb.java
@@ -0,0 +1,13 @@
+package apap.tutorial.manpromanpro.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import apap.tutorial.manpromanpro.model.Proyek;
+
+@Repository
+public interface ProyekDb extends JpaRepository<Proyek, UUID> {
+
+}
